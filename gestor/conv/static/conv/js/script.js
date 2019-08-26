4d5effe90fb068c47b1dbab615809dd319f360cf
Larreahena0 <santiago.larrea@udea.edu.co>
2019-08-26 15:57:08 -0500

    Pasando a visual studio.

--- gestor/conv/static/conv/js/script.js
+++ gestor/conv/static/conv/js/script.js
@@ -1,27 +1,27 @@
 var count = 1;
 
 function add(){
-  var formulario = document.getElementById('campos');
-  var description = document.createElement('input');
-  var input = document.createElement('input')
-  var contador = document.getElementById('contador')
+    var formulario = document.getElementById('campos');
+    var description = document.createElement('input');
+    var input = document.createElement('input')
+    var contador = document.getElementById('contador')
 
-  description.type = 'text';
-  description.id = "text_" + count.toString();
-  description.name = "text_" + count.toString();
-  description.required = true;
+    description.type = 'text';
+    description.id = "text_" + count.toString();
+    description.name = "text_" + count.toString();
+    description.required = true;
 
-  input.type = 'file';
-  input.accept = 'application/pdf';
-  input.id = "doc_" + count.toString();
-  input.name = "doc_" + count.toString();
-  input.required = true;
+    input.type = 'file';
+    input.accept = 'application/pdf';
+    input.id = "doc_" + count.toString();
+    input.name = "doc_" + count.toString();
+    input.required = true;
 
-  contador.value = count;
+    contador.value = count;
 
-  formulario.appendChild(description);
-  formulario.appendChild(input);
-  formulario.appendChild(document.createElement('br'))
+    formulario.appendChild(description);
+    formulario.appendChild(input);
+    formulario.appendChild(document.createElement('br'))
 
-  count += 1;
+    count += 1;
 }
